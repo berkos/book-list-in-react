79e6c444448fcbfbd4ef1d993a6995c1a7f49865
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-16 17:44:18 +0100

    Add pages in the books

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'Javascript: The Good Parts', pages: 234},
+    {title: 'Harry Potter', pages: 134},
+    {title: 'The Dark Tower', pages: 274},
+    {title: 'Eloquent Ruby', pages: 534}
   ]
 }
